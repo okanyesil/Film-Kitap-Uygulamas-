9898700cd4a73b144a5bb6b5284cf3a3d36b7369
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-01-02 23:52:25 +0300

    Admin controller index sayfası hazırlandı

---
+++ WebOdev/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebOdev.Models;
+
+namespace WebOdev.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly KitapDBContext _context;
+        public AdminController(KitapDBContext kitap)
+        {
+            _context = kitap;
+
+        }
+        public IActionResult Index()
+        {
+            var tumIcerik = new KitapYazarYorumKategoriModel();
+            tumIcerik.Kitaplar = _context.Kitaps.ToList();
+            tumIcerik.Yazarlar = _context.Yazars.ToList();
+            tumIcerik.Yorumlar = _context.Yorums.ToList();
+            tumIcerik.Kategoriler = _context.Kategoris.ToList();
+            return View(tumIcerik);
+        }
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+        public IActionResult Guncelle()
+        {
+            return View();
+        }
+        public IActionResult Sil()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebOdev/Models/KitapYazarYorumKategoriModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebOdev.Models
+{
+    public class KitapYazarYorumKategoriModel
+    {
+        public IEnumerable<Kitap> Kitaplar { get; set; }
+        public IEnumerable<Yazar> Yazarlar { get; set; }
+        public IEnumerable<Yorum> Yorumlar { get; set; }
+        public IEnumerable<Kategori> Kategoriler { get; set; }
+    }
+}
